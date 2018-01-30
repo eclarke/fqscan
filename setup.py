8b9bf780016f8593bfa5b7b56de1e59202ca07a5
Erik Clarke <erikclarke@fastmail.com>
2018-01-30 17:44:54 -0500

    add requirements

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     description="Scan FASTQ files quickly for a given set of sequences.",
     url='https://github.com/eclarke/fqscan',
     packages=['fqscan'],
+    requirements=['pysam'],
     entry_points={
         'console_scripts': [
             'fqscan=fqscan.main:main',
